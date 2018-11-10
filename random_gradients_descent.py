fc97bc2aac5873297b74bfe39e59b109b314438e
tujinliangtester <tujinliangtester@163.com>
2018-11-10 20:26:44 +0800

    linear_study_0

--- random_gradients_descent.py
+++ random_gradients_descent.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 X=np.random.rand(100,1)
 X_b=np.c_[np.ones((100,1)),X]
@@ -10,6 +11,7 @@ n_epochs=2000
 m=100
 learn_rate=0.1
 t0,t1=10,50
+mini_bat=5
 def learning_schedule(t):
     return t0/(t1+t)
 
@@ -18,14 +20,22 @@ theta=np.random.randn(2,1)
 for epoch in range(n_epochs):
     for j in range(m):
         #random line
-        random_index=np.random.randint(m)
-        xi=X_b[random_index:random_index+1]
-        yi=Y[random_index:random_index+1]
-        gradient=xi.T.dot(xi.dot(theta)-yi)
+        random_index=np.random.randint(m-mini_bat)
+        xi=X_b[random_index:random_index+mini_bat]
+        yi=Y[random_index:random_index+mini_bat]
+        gradient=xi.T.dot(xi.dot(theta)-yi)/mini_bat
 
         learn_rate=j+epoch*m
         learn_rate=learning_schedule(learn_rate)
         theta=theta-gradient*learn_rate
-        epsi=X_b.dot(theta)-Y
+
 print(theta)
 print(flag)
+
+X_test=np.array([[0],[1]])
+X_test_b=np.c_[np.ones((2,1)),X_test]
+Y_test=X_test_b.dot(theta)
+plt.plot(X,Y,'b.')
+plt.plot(X_test,Y_test,'r-')
+plt.axis([0,1,0,15])
+plt.show()
---
+++ ridge_regression.py
@@ -0,0 +1,32 @@
+import numpy
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import SGDRegressor
+import matplotlib.pyplot as plt
+X=numpy.random.rand(100,1)
+Y=3+4*X+numpy.random.randn(100,1)
+
+
+# Ridge
+ridge_model=Ridge(alpha=0.1,solver='sag')
+ridge_model.fit(X,Y)
+print(ridge_model.intercept_)
+print(ridge_model.coef_)
+X_test=numpy.array([[1.5],[0.5],[0]])
+Y_predict=ridge_model.predict(X_test)
+print(ridge_model.predict(X_test))
+plt.plot(X,Y,'b.')
+plt.plot(X_test,Y_predict,'r-')
+
+
+print('SGDG')
+SGDRegressor_model=SGDRegressor(penalty='l2',alpha=0.1,tol=1e-3)
+SGDRegressor_model.fit(X,Y.ravel())
+print(SGDRegressor_model.intercept_)
+print(SGDRegressor_model.coef_)
+# X_test2=numpy.array([[1.5]])
+Y_predict2=SGDRegressor_model.predict(X_test)
+print(SGDRegressor_model.predict(X_test))
+
+plt.plot(X_test,Y_predict2,'g-')
+plt.axis([0,2,0,10])
+plt.show()
