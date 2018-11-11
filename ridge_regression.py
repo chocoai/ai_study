b3f84424d76ef072daa1e904a641b3d7789f44f9
tujinliangtester <tujinliangtester@163.com>
2018-11-11 20:16:33 +0800

    linear_study_0

--- ridge_regression.py
+++ ridge_regression.py
@@ -5,8 +5,6 @@ import matplotlib.pyplot as plt
 X=numpy.random.rand(100,1)
 Y=3+4*X+numpy.random.randn(100,1)
 
-
-
 plt.plot(X,Y,'b.')
 
 
@@ -34,15 +32,16 @@ plt.plot(X_test,Y_predict2,'g-')
 plt.axis([0,2,0,10])
 # plt.show()
 
-# for two vars error!!!
+# for two vars is ok!
+#have to check the Y_2 is must made by 2D form X_2,the ':' is expected!!!
 print('for two vars')
 X_2=numpy.random.rand(10,2)
-Y_2=3+4*X_2[:,0]+5*X_2[:,1]+numpy.random.randn(10,1)
-print(X_2[:,0])
-
+Y_2=3+4*X_2[:,0:1]+5*X_2[:,1:]+numpy.random.randn(10,1)
 ridge_model=Ridge(solver='sag',max_iter=1000)
 ridge_model.fit(X_2,Y_2)
+print(222222222222222222222222222)
 print(ridge_model.intercept_)
+print(333)
 print(ridge_model.coef_)
 X_test=numpy.array([[2]])
 X_test=numpy.c_[numpy.ones((1,1)),X_test]
