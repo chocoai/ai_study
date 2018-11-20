5a7a4bd6ec7d06fdb6762b1e7faed720e79349ae
tujinliangtester <tujinliangtester@163.com>
2018-11-20 16:31:37 +0800

    mnist

--- MNIST/mnist_study.py
+++ MNIST/mnist_study.py
@@ -2,6 +2,10 @@ import os
 import struct
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score,precision_score,recall_score,precision_recall_curve,f1_score,roc_curve,roc_auc_score
+from sklearn.externals import joblib
+
 def load_mnist(path, kind='train'):
     """Load MNIST data from `path`"""
     labels_path = os.path.join(path,
@@ -23,7 +27,45 @@ def load_mnist(path, kind='train'):
     return images, labels
 
 if __name__=='__main__':
+
     images, labels=load_mnist('.',kind='train')
     X_train,y_train=images, labels
 
-    print()
+    images, labels = load_mnist('.', kind='t10k')
+    X_test, y_test = images, labels
+    y_train = (y_train == 5)
+    y_test = (y_train == 5)
+    '''
+    # x=X_train[3600].reshape(28,28)
+    # plt.imshow(x)
+    # plt.axis('off')
+    # plt.show()
+    
+    print(y_train)
+    
+
+    lclf=LogisticRegression(max_iter=100,solver='sag')
+    lclf.fit(X_train,y_train)
+    joblib.dump(lclf,'lclf.pkl')
+    print('fit is done')
+    '''
+    lclf=joblib.load('lclf.pkl')
+    y_valid=lclf.predict(X_train)
+
+    acc=accuracy_score(y_train,y_valid)
+    print(acc)
+
+    precision=precision_score(y_train,y_valid)
+    print(precision)
+
+    recall=recall_score(y_train,y_valid)
+    print(recall)
+
+    f1=f1_score(y_train,y_valid)
+    print(f1)
+
+    fpr, tpr, thresholds = roc_curve(y_train, y_valid)
+    print(fpr,tpr,thresholds)
+
+    plt.plot(fpr,tpr,'b.')
+    plt.show()
