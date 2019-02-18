14c73125fad5ea2c4d16d3a5e90462a997760c7d
tujinliangtester <tujinliangtester@163.com>
2019-02-18 22:52:39 +0800

    tensorboard再次复习 1.学习时需要记笔记 2.今天的排查浪费很多时间，结果只是文件目录错误，工具有坑

--- tensorflow_study/tensor_board/line_model.py
+++ tensorflow_study/tensor_board/line_model.py
@@ -25,14 +25,16 @@ with tf.name_scope('y'):
 
 with tf.name_scope('var'):
     w=tf.Variable(tf.random_normal((1,1)),name='w')
+    tf.summary.histogram(name='w',values=w)
     b=tf.Variable(tf.zeros([1]),name='b')
+    tf.summary.histogram(name='b',values=b)
 
 with tf.name_scope('y_'):
     y_=tf.matmul(X,w)+b
 
 with tf.name_scope('loss'):
     loss=tf.reduce_mean(tf.square(y-y_))
-
+    tf.summary.scalar(name='loss',tensor=loss)
 with tf.name_scope('optimizer'):
     optimizer=tf.train.GradientDescentOptimizer(learning_rate=0.01)
 
@@ -41,13 +43,19 @@ with tf.name_scope('train'):
 
 init=tf.global_variables_initializer()
 n_epochs=10000
+
 with tf.Session() as sess:
-    tf.summary.FileWriter('logs/',sess.graph)
+    merge=tf.summary.merge_all()
+    fw=tf.summary.FileWriter('logs/',sess.graph)
     init.run()
     for n in range(n_epochs):
-        train.run(feed_dict={X:X_data})
+        sess.run([train],feed_dict={X:X_data})
+        # fw.run(feed_dict={X:X_data})
         if(n%100==0):
+            _,summary_str=sess.run([train,merge], feed_dict={X: X_data})
+            fw.add_summary(summary=summary_str,global_step=n)
             print('第',n,'次：')
             print(w.eval())
             print(b.eval())
             print()
+    fw.close()
